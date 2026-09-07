5c7b1c4094af67d600e62c8444ff03812d1beb1e
marcelozramos-zrti <mramos.zrti@gmail.com>
2026-09-07 15:45:43 -0300

    chore: update default server port to 3002

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import { AuditLog, AuditAction, AppModule, AuditResult, Role } from './src/types
 
 async function startServer() {
   const app = express();
-  const PORT = Number(process.env.APP_PORT) || 3000;
+  const PORT = Number(process.env.APP_PORT) || 3002;
 
   app.use(cors());
   app.use(express.json({ limit: '15mb' }));
